3084b55102c3fac495f8817e8b5cf11266841874
Caroline Roy <caroline.roy.dev@gmail.com>
2024-02-28 16:00:40 +0100

    Personal solution

---
+++ src/app/02-interview/Comments.js
@@ -0,0 +1,16 @@
+import { getComments } from '@/helpers/interview-helpers'
+import Comment from '@/components/Comment'
+
+async function Comments() {
+  const comments = await getComments()
+
+  return (
+    <>
+      {comments.map(comment => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </>
+  )
+}
+
+export default Comments
